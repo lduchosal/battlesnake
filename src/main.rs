821ec3e5787ab2542885b2f6a3f87661cd7c51a7
Luc Duchosal <q@MacBook-Pro-de-Luc.local>
2019-03-09 18:25:19 +0100

    check_walls

--- src/main.rs
+++ src/main.rs
@@ -2,7 +2,6 @@ extern crate tiny_http;
 extern crate serde_json;
 
 use serde::{Deserialize, Serialize};
-use serde_json::Result;
 
 fn main() {
     use tiny_http::{Server, Response};
@@ -13,25 +12,15 @@ fn main() {
 
     let config = Config {
         color: "#ff00ff".to_string(),
-        head: "bendr".to_string(),
-        tail: "pixel".to_string()
+        head: HeadType::SandWorm,
+        tail: TailType::BlockBum
         };
 
-    let right = Move { dir: "right".to_string() };
-    let left = Move { dir: "left".to_string() }; 
-    let up = Move { dir: "up".to_string() }; 
-    let down = Move { dir: "down".to_string() };
-
-    let bye = Say { say: "bye".to_string() };
-    let hello = Say { say: "hello".to_string() };
-    let pong = Say { say: "pong".to_string() };
+    let bye = Say::Bye;
+    let hello = Say::Hello;
+    let pong = Say::Pong;
 
     let sconfig = serde_json::to_string(&config).unwrap();
-
-    let sright = serde_json::to_string(&right).unwrap();
-    let sleft = serde_json::to_string(&left).unwrap();
-    let sup = serde_json::to_string(&up).unwrap();
-    let sdown = serde_json::to_string(&down).unwrap();
 
     let sbye = serde_json::to_string(&bye).unwrap();
     let shello = serde_json::to_string(&hello).unwrap();
@@ -40,42 +29,101 @@ fn main() {
 
     loop {
 
-        println!("receving");
+        println!("--");
 
         let mut rq = match server.recv() {
             Ok(rq) => rq,
             Err(_) => break
         };
 
-        println!(" rq method: {:?}", rq.method());
+        // println!(" rq method: {:?}", rq.method());
         println!(" rq url: {:?}", rq.url());
         // println!(" rq headers: {:?}", rq.headers());
-        println!(" rq body_length: {:?}", rq.body_length());
+        // println!(" rq body_length: {:?}", rq.body_length());
 
         let mut content = String::new();
         rq.as_reader().read_to_string(&mut content).unwrap();
 
         println!(" rq content: {:?}", content);
-        let g: Option<Game> = match serde_json::from_str(content.as_ref()) {
-            Ok(g) => Some(g),
-            Err(_) => None,
-        };
+
+        let mut result: String;
 
         let message = match rq.url() {
-            "/start" => sconfig.clone(),
-            "/move" => sdown.clone(),
-            "/end" => sbye.clone(),
-            "/ping" => spong.clone(),
-            _ => shello.clone()
+            "/start" => &sconfig,
+            "/move" => {
+                
+                let next_move = play(content.as_ref());
+                result = serde_json::to_string(&next_move).unwrap();
+                &result
+            },
+            "/end" => &sbye,
+            "/ping" => &spong,
+            _ => &shello
         };
 
-        println!("response: {}", message);
-        let response = Response::from_string(message);
+        println!("message: {}", message);
+        let response = Response::from_string(message.clone());
         match rq.respond(response) {
             Ok(()) => {},
             Err(err) => println!("Error: {}", err),
         }
     }
+}
+
+fn play(content: &str) -> Move {
+
+    let g: Game = serde_json::from_str(content).expect("Unable to deserialize game");
+
+    let head = &g.you.body[0];
+
+    let mut possibles: Vec<Possible> = Vec::new();
+    let up = Possible { point: Point { x: head.x, y: head.y -1 }, dir: Move::Up, value: 10 };
+    let down = Possible { point: Point { x: head.x, y: head.y +1 }, dir: Move::Down, value: 10 };
+    let left = Possible { point: Point { x: head.x -1, y: head.y }, dir: Move::Left, value: 10 };
+    let right = Possible { point: Point { x: head.x +1, y: head.y }, dir: Move::Right, value: 10 };
+
+    possibles.push(up);
+    possibles.push(down);
+    possibles.push(left);
+    possibles.push(right);
+
+    check_walls(&g, &mut possibles);
+    dump_results(&possibles);
+
+    possibles.sort_by(|a, b| b.value.cmp(&a.value));
+    let bestfit = possibles.first().unwrap();
+    bestfit.dir.clone()
+}
+
+fn dump_results(possibles: &Vec<Possible>) {
+    for p in possibles {
+        println!("possible: {:#?}", p);
+    }
+}
+
+fn check_walls(game: &Game, possibles: &mut Vec<Possible>) {
+
+    for p in possibles {
+        if p.point.x < 0 {
+            p.value -= 1;
+        }
+        if p.point.y < 0 {
+            p.value -= 1;
+        }
+        if p.point.x > (game.board.width as i8) {
+            p.value -= 1;
+        }
+        if p.point.x > (game.board.height as i8) {
+            p.value -= 1;
+        }
+    }
+}
+
+#[derive(Debug)]
+pub struct Possible {
+    point: Point,
+    dir: Move,
+    value: u8
 }
 
 #[derive(Serialize, Deserialize)]
@@ -84,22 +132,64 @@ pub struct Config {
     color: String,
 
     #[serde(rename = "headType")]
-    head: String,
+    head: HeadType,
 
     #[serde(rename = "tailType")]
-    tail: String,
+    tail: TailType,
 }
 
 #[derive(Serialize, Deserialize)]
-pub struct Move {
-    #[serde(rename = "move")]
-    dir: String,
+#[serde(rename_all = "kebab-case")]
+enum HeadType {
+    Beluga,
+    Bendr,
+    Dead,
+    Evil,
+    Fang,
+    Pixel,
+    Regular,
+    Safe,
+    SandWorm,
+    Shades,
+    Silly,
+    Smile,
+    Tongue,
 }
 
 #[derive(Serialize, Deserialize)]
-pub struct Say {
-    #[serde(rename = "say")]
-    say: String,
+#[serde(rename_all = "kebab-case")]
+enum TailType {
+    BlockBum,
+    Bolt,
+    Curled,
+    FatRattle,
+    Freckled,
+    Hook,
+    Pixel,
+    Regular,
+    RoundBum,
+    Sharp,
+    Skinny,
+    SmallRattle
+}
+
+#[derive(Serialize, Deserialize, Clone, Debug)]
+#[serde(rename_all = "lowercase")]
+#[serde(tag = "move")]
+enum Move {
+    Up,
+    Down,
+    Left,
+    Right
+}
+
+#[derive(Serialize, Deserialize)]
+#[serde(rename_all = "lowercase")]
+#[serde(tag = "say")]
+enum Say {
+    Bye,
+    Hello,
+    Pong
 }
 
 #[derive(Serialize, Deserialize)]
@@ -108,7 +198,7 @@ pub struct Game {
     game: GameId,
 
     #[serde(rename = "turn")]
-    turn: i64,
+    turn: u8,
 
     #[serde(rename = "board")]
     board: Board,
@@ -120,25 +210,32 @@ pub struct Game {
 #[derive(Serialize, Deserialize)]
 pub struct Board {
     #[serde(rename = "height")]
-    height: i64,
+    height: u8,
 
     #[serde(rename = "width")]
-    width: i64,
+    width: u8,
 
     #[serde(rename = "food")]
-    food: Vec<Food>,
+    food: Vec<Point>,
 
     #[serde(rename = "snakes")]
     snakes: Vec<Snake>,
 }
 
-#[derive(Serialize, Deserialize)]
-pub struct Food {
+#[derive(Serialize, Deserialize, Debug)]
+pub struct Point {
     #[serde(rename = "x")]
-    x: i64,
+    pub x: i8,
 
     #[serde(rename = "y")]
-    y: i64,
+    pub y: i8,
+}
+
+impl PartialEq for Point {
+    fn eq(&self, other: &Point) -> bool {
+        self.x == other.x && 
+        self.y == other.y
+    }
 }
 
 #[derive(Serialize, Deserialize)]
@@ -150,10 +247,10 @@ pub struct Snake {
     name: String,
 
     #[serde(rename = "health")]
-    health: i64,
+    health: u8,
 
     #[serde(rename = "body")]
-    body: Vec<Food>,
+    body: Vec<Point>,
 }
 
 #[derive(Serialize, Deserialize)]
