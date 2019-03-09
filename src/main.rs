8f287d12fb66d3d359d36ff660df08503bf558d3
Luc Duchosal <q@MacBook-Pro-de-Luc.local>
2019-03-09 23:35:28 +0100

    markdavid

--- src/main.rs
+++ src/main.rs
@@ -6,12 +6,12 @@ use serde::{Deserialize, Serialize};
 fn main() {
     use tiny_http::{Server, Response};
 
-    let server = Server::http("0.0.0.0:6602").unwrap();
+    let server = Server::http("0.0.0.0:6601").unwrap();
     let port = server.server_addr().port();
     println!("Now listening on port {}", port);
 
     let config = Config {
-        color: "#ff00ff".to_string(),
+        color: "#fe00fe".to_string(),
         head: HeadType::SandWorm,
         tail: TailType::BlockBum
         };
@@ -74,7 +74,19 @@ fn play(content: &str) -> Move {
 
     let g: Game = serde_json::from_str(content).expect("Unable to deserialize game");
 
-    let head = &g.you.body[0];
+    let mut possibles = possibles(&g.you);
+    check_walls(&g, &mut possibles);
+    check_snakes(&g, &mut possibles);
+    check_collisions(&g, &mut possibles);
+    dump_results(&possibles);
+
+    possibles.sort_by(|a, b| b.value.cmp(&a.value));
+    let bestfit = possibles.first().unwrap();
+    bestfit.dir.clone()
+}
+
+fn possibles(snake: &Snake) -> Vec<Possible> {
+    let head = &snake.body[0];
 
     let mut possibles: Vec<Possible> = Vec::new();
     let up = Possible { point: Point { x: head.x, y: head.y -1 }, dir: Move::Up, value: 10 };
@@ -87,13 +99,7 @@ fn play(content: &str) -> Move {
     possibles.push(left);
     possibles.push(right);
 
-    check_walls(&g, &mut possibles);
-    check_snakes(&g, &mut possibles);
-    dump_results(&possibles);
-
-    possibles.sort_by(|a, b| b.value.cmp(&a.value));
-    let bestfit = possibles.first().unwrap();
-    bestfit.dir.clone()
+    possibles
 }
 
 fn dump_results(possibles: &Vec<Possible>) {
@@ -106,16 +112,16 @@ fn check_walls(game: &Game, possibles: &mut Vec<Possible>) {
 
     for p in possibles {
         if p.point.x < 0 {
-            p.value -= 1;
+            p.value -= 10;
         }
         if p.point.y < 0 {
-            p.value -= 1;
+            p.value -= 10;
         }
         if p.point.x >= (game.board.width as i8) {
-            p.value -= 1;
+            p.value -= 10;
         }
         if p.point.y >= (game.board.height as i8) {
-            p.value -= 1;
+            p.value -= 10;
         }
     }
 }
@@ -126,6 +132,21 @@ fn check_snakes(game: &Game, possibles: &mut Vec<Possible>) {
         for s in &game.board.snakes {
             for b in &s.body {
                 if p.point == *b {
+                    p.value -= 10 ;
+                }
+            }
+        }
+    }
+}
+
+fn check_collisions(game: &Game, ps: &mut Vec<Possible>) {
+
+    for p in ps {
+        for s in &game.board.snakes {
+
+            let pothers = possibles(&s);
+            for po in pothers {
+                if p.point == po.point {
                     p.value -= 1 ;
                 }
             }
@@ -137,7 +158,7 @@ fn check_snakes(game: &Game, possibles: &mut Vec<Possible>) {
 pub struct Possible {
     point: Point,
     dir: Move,
-    value: u8
+    value: i32
 }
 
 #[derive(Serialize, Deserialize)]
